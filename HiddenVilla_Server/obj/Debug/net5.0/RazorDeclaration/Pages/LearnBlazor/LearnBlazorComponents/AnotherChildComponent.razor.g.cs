37f67905664384e1b62e20e0687ae8cb57d428d0
Aybars Acar <aybars.dev@gmail.com>
2021-01-28 17:02:51 +1100

    Initial Migration Done

--- HiddenVilla_Server/obj/Debug/net5.0/RazorDeclaration/Pages/LearnBlazor/LearnBlazorComponents/AnotherChildComponent.razor.g.cs
+++ HiddenVilla_Server/obj/Debug/net5.0/RazorDeclaration/Pages/LearnBlazor/LearnBlazorComponents/AnotherChildComponent.razor.g.cs
@@ -13,84 +13,84 @@ namespace HiddenVilla_Server.Pages.LearnBlazor.LearnBlazorComponents
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 1 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 2 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 3 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 4 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 5 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 6 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 7 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 8 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 9 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using HiddenVilla_Server;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 10 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using HiddenVilla_Server.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 11 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using HiddenVilla_Server.Pages.LearnBlazor.LearnBlazorComponents;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/_Imports.razor"
+#line 12 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/_Imports.razor"
 using HiddenVilla_Server.Model;
 
 #line default
@@ -104,7 +104,7 @@ using HiddenVilla_Server.Model;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 8 "/Users/aybarsacar/Desktop/cs/HiddenVilla/HiddenVilla_Server/Pages/LearnBlazor/LearnBlazorComponents/AnotherChildComponent.razor"
+#line 8 "/Users/aybarsacar/Desktop/cs/DotnetBlazorLearningMaterial/HiddenVilla_Server/Pages/LearnBlazor/LearnBlazorComponents/AnotherChildComponent.razor"
  
     // [Parameter]
     // public string Placeholder { get; set; }
